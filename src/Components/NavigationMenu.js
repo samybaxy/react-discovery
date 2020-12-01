63f55df2ee04e1c1c3e66e15656bb073c6bdbaf2
Samybaxy <upwork@samybaxy.net>
2020-12-01 12:00:39 +0100

    Axios install and setup

--- src/Components/NavigationMenu.js
+++ src/Components/NavigationMenu.js
@@ -13,12 +13,22 @@ function NavigationMenu(props) {
                         onClick={props.closeMenu}
                     >Home</Link>
                 </li>
+
                 <li>
                     <Link 
                         to="/about"
                         className="text-blue-500 py-3 border-b block"
                         onClick={props.closeMenu}
-                    >About</Link></li>
+                    >About</Link>
+                </li>
+
+                <li>
+                    <Link 
+                        to="/product"
+                        className="text-blue-500 py-3 border-b block"
+                        onClick={props.closeMenu}
+                    >Product</Link>
+                </li>
             </ul>
         </div>
     )
