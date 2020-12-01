215d618f2c4b21e2d6915a719aa0671fb3f4f3f2
Samybaxy <upwork@samybaxy.net>
2020-12-01 02:30:07 +0100

    Content views

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import Header from './Components/Header';
-import Footer from './Components/Footer';
+
 import {
 	BrowserRouter as Router,
 	Switch,
 	Route
 } from 'react-router-dom';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Home from './Views/Home';
+import About from './Views/About';
 
 function App() {
   return (
     <Router>
 		<Header />
 
-		<Switch>
+		<div className="p-3">
+			<Switch>
 			<Route exact path="/">
-				This is the homepage
+				<Home />
 			</Route>
+			
 			<Route path="/about">
-				This is the About Us page
+				<About />
 			</Route>
-		</Switch>
+			</Switch>
+		</div>
 
 		<Footer />
     </Router>
---
+++ src/Views/Home.js
@@ -0,0 +1,11 @@
+
+function Home() {
+    return (
+        <div>
+            <h1 className="font-bold text-2xl">Home</h1>
+            <p>This is the home page content</p>
+        </div>
+    )
+}
+
+export default Home;
