dac6811c4000c00af50fe4e437509d10d31aa035
Samybaxy <upwork@samybaxy.net>
2020-12-01 12:22:36 +0100

    Added css loader

--- src/Views/Product.js
+++ src/Views/Product.js
@@ -1,43 +1,57 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import Loader from '../Components/Loader';
 
 function Product() {
     const { id } = useParams();
     const url = `https://5fc59f034931580016e3bf0d.mockapi.io/api/v1/products/${id}`;
-    const [product, setProduct] = useState(null);
+    const [product, setProduct] = useState({
+        loading: false,
+        data: null
+    });
 
     let content = null;
 
     useEffect(() => {
-    
+        setProduct({
+            loading: true,
+            data: null
+        })
         axios.get(url)
             .then(response => {
-                setProduct(response.data)
+                setProduct({
+                    loading: false,
+                    data: response.data
+                })
             })
 
     }, [url] );
 
-    if (product) {
+    if (product.loading) {
+        content = <Loader />
+    }
+    
+    if (product.data) {
         content =
         <div>
             <h1 className="text-2xl font-bold mb-3">
-                {product.name}
+                {product.data.name}
             </h1>
 
             <div>
                 <img 
-                    src={product.images[0].imageUrl} 
-                    alt={product.name} 
+                    src={product.data.images[0].imageUrl} 
+                    alt={product.data.name} 
                 />
             </div>
 
             <div className="font-bold text-xl mb-3">
-                $ {product.price}
+                $ {product.data.price}
             </div>
 
             <div>
-                {product.description}
+                {product.data.description}
             </div>
         </div>
     }
